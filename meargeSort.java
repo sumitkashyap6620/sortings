9992a721f44b4dbaf10b200f2ba4b2b55b4d89cb
sumit kashyap <sumit.kashyap.6620@gmail.com>
2025-08-20 02:36:07 +0530

    refined the programe

--- meargeSort.java
+++ meargeSort.java
@@ -1,11 +1,9 @@
 import java.util.Scanner;
 public class meargeSort{
     public static void display(int arr[]){
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) 
             System.out.print(arr[i] +" ");
-           
-        }
-    }
+     }
     public static void mearge(int[] arr , int l , int mid , int r ){
        int n1 = mid-l+1 ;
        int n2 = r-mid ;
@@ -38,18 +36,16 @@ public class meargeSort{
             int mid = (l+r)/2 ;
             meargesort(arr,l,mid);
             meargesort(arr , mid+1 , r);
-            mearge(arr , l , mid , r);
-       
+            mearge(arr , l , mid , r);   
     }
     public static void main (String args[]){
         System.out.print("give length :- ");
         Scanner sc = new Scanner (System.in);
         int arr[] = new int[sc.nextInt()];
        System.out.println("give elements :-");
-       for (int i = 0; i < arr.length; i++) {
+       for (int i = 0; i < arr.length; i++) 
           arr[i] = sc.nextInt(); 
-       }
-       meargesort(arr,0,arr.length-1);
+        meargesort(arr,0,arr.length-1);
         System.out.print("shorted array :");
           display(arr);
     }

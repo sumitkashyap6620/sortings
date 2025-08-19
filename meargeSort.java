ab87fb3b863f4096fd413e6aff2120887537ad41
sumit kashyap <sumit.kashyap.6620@gmail.com>
2025-08-20 02:26:21 +0530

    completed

--- meargeSort.java
+++ meargeSort.java
@@ -7,15 +7,52 @@ public class meargeSort{
         }
     }
     public static void sort(int[] arr , int l , int mid , int r ){
-
+       int n1 = mid-l+1 ;
+       int n2 = r-mid ;
+       int w = l ;
+       int[] left = new int[n1];
+       int[] right = new int[n2];
+       for (int i=0 ; i < n1 ; i++){
+        left[i] = arr[l++];
+       }
+       for (int i=0 ; i < n2 ; i++){
+        right[i] = arr[mid+1];
+        mid++ ;
+       }
+       l = w ;
+       int i = 0 ; 
+       int j = 0 ;
+       int k = l ;
+       while ( i < n1 && j < n2 ){
+        if (left[i]<=right[j]){
+        arr[k] = left[i] ;
+        k++ ; 
+        i++ ;
+    }
+       else{
+         arr[k] = right[j] ;
+         k++ ;
+         j++ ;
+       }
+       }
+       while(i<n1){
+        arr[k] = left[i];
+        k++ ;
+        i++ ;
+    }
+    while(j<n2){
+        arr[k] = right[j];
+        k++ ;
+        j++ ;
+    }
     }
     public static void meargeS(int arr[] , int l , int r ){
+            if(l>=r)return;
             int mid = (l+r)/2 ;
             meargeS(arr,l,mid);
             meargeS(arr , mid+1 , r);
             sort(arr , l , mid , r);
-        System.out.print("shorted array :");
-          display(arr);
+       
     }
     public static void main (String args[]){
         System.out.print("give length :- ");
@@ -26,5 +63,7 @@ public class meargeSort{
           arr[i] = sc.nextInt(); 
        }
        meargeS(arr,0,arr.length-1);
+        System.out.print("shorted array :");
+          display(arr);
     }
 }

64ca6e8c32685262fe10bb8cad538a51fc2eb838
sumit kashyap <sumit.kashyap.6620@gmail.com>
2025-08-20 02:32:57 +0530

    completed

--- meargeSort.java
+++ meargeSort.java
@@ -6,15 +6,14 @@ public class meargeSort{
            
         }
     }
-    public static void sort(int[] arr , int l , int mid , int r ){
+    public static void mearge(int[] arr , int l , int mid , int r ){
        int n1 = mid-l+1 ;
        int n2 = r-mid ;
        int w = l ;
        int[] left = new int[n1];
        int[] right = new int[n2];
-       for (int i=0 ; i < n1 ; i++){
+       for (int i=0 ; i < n1 ; i++)
         left[i] = arr[l++];
-       }
        for (int i=0 ; i < n2 ; i++){
         right[i] = arr[mid+1];
         mid++ ;
@@ -24,34 +23,22 @@ public class meargeSort{
        int j = 0 ;
        int k = l ;
        while ( i < n1 && j < n2 ){
-        if (left[i]<=right[j]){
-        arr[k] = left[i] ;
-        k++ ; 
-        i++ ;
-    }
-       else{
-         arr[k] = right[j] ;
-         k++ ;
-         j++ ;
+        if (left[i]<=right[j])
+        arr[k++] = left[i++] ;
+       else
+         arr[k++] = right[j++] ;
        }
-       }
-       while(i<n1){
-        arr[k] = left[i];
-        k++ ;
-        i++ ;
+       while(i<n1)
+        arr[k++] = left[i++];
+    while(j<n2)
+        arr[k++] = right[j++];
     }
-    while(j<n2){
-        arr[k] = right[j];
-        k++ ;
-        j++ ;
-    }
-    }
-    public static void meargeS(int arr[] , int l , int r ){
+    public static void meargesort(int arr[] , int l , int r ){
             if(l>=r)return;
             int mid = (l+r)/2 ;
-            meargeS(arr,l,mid);
-            meargeS(arr , mid+1 , r);
-            sort(arr , l , mid , r);
+            meargesort(arr,l,mid);
+            meargesort(arr , mid+1 , r);
+            mearge(arr , l , mid , r);
        
     }
     public static void main (String args[]){
@@ -62,7 +49,7 @@ public class meargeSort{
        for (int i = 0; i < arr.length; i++) {
           arr[i] = sc.nextInt(); 
        }
-       meargeS(arr,0,arr.length-1);
+       meargesort(arr,0,arr.length-1);
         System.out.print("shorted array :");
           display(arr);
     }
